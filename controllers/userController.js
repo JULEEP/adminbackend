5fb7dff51ac22ed6555f8b631dad6f5348ad856d
JULEEP <juleeperween34@gmail.com>
2026-04-11 18:34:31 +0530

    adminback

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,20 +2,21 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
 import generateToken from '../utils/generateToken.js';
 
-// AUTH
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  // Find user by email
   const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
+  // Plain text password match
+  if (user && user.password === password) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       role: user.role,
       accessibleProducts: user.accessibleProducts,
-      token: generateToken(user._id),
+      token: generateToken(user._id), // JWT token
     });
   } else {
     res.status(401);
